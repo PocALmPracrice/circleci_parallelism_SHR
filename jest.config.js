850e43eb67a7648c965e07a18cde36a0aab58746
shadabmalikshah <97905885+shadabmalikshah@users.noreply.github.com>
2024-10-15 18:28:25 +0530

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    testEnvironment: "node",
-    testTimeout: 10000
-  };
-  
+  // Other configurations...
+  testResultsProcessor: 'jest-junit', // if you are using jest-junit for JUnit reports
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'test-results', outputName: 'results.xml' }]
+  ],
+};
